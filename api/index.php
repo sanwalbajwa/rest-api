189bfce484d44ae2547da624f1cd3c627e7b3b88
sanwalbajwa <sanwalbajwa6026@gmail.com>
2024-09-19 15:33:43 +0500

    initial code

--- api/index.php
+++ api/index.php
@@ -4,3 +4,9 @@ $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $parts = explode("/", $path);
 var_dump ($parts);
 echo $_SERVER["REQUEST_METHOD"];
+
+
+require dirname(__DIR__) . "/src/TaskController.php";
+
+$controller = new TaskController;
+$controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
