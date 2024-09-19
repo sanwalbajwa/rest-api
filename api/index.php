dd34e5ff72203082591f759e41a3e8cdad8cc79c
sanwalbajwa <sanwalbajwa6026@gmail.com>
2024-09-19 15:40:32 +0500

    initial code

--- api/index.php
+++ api/index.php
@@ -1,11 +1,18 @@
 <?php
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
 $parts = explode("/", $path);
-var_dump ($parts);
-echo $_SERVER["REQUEST_METHOD"];
 
+$resource = $parts[2];
 
+$id = $parts[3] ?? null;
+
+if ($resource != "tasks") {
+    
+    http_response_code(404);
+    exit;
+}
 require dirname(__DIR__) . "/src/TaskController.php";
 
 $controller = new TaskController;
